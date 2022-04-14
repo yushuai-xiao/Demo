c2a6ba8aebb97beef738ad982788fd42968a7fac
xushuai <851647744@qq.com>
2022-04-14 16:28:37 +0800

    second commit

---
+++ vue_demo/src/components/HelloWorld.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container">
+    <el-table :data="tableData" border style="width: 70%">
+      <el-table-column fixed prop="date" label="日期" width="150">
+      </el-table-column>
+      <el-table-column prop="name" label="姓名" width="120"> </el-table-column>
+      <el-table-column prop="province" label="省份" width="120">
+      </el-table-column>
+      <el-table-column prop="city" label="市区" width="120"> </el-table-column>
+      <el-table-column prop="address" label="地址" width="300">
+      </el-table-column>
+      <el-table-column prop="zip" label="邮编" width="120"> </el-table-column>
+      <el-table-column fixed="right" label="操作" width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small"
+            >查看</el-button
+          >
+          <el-button type="text" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  methods: {
+    handleClick(row) {
+      console.log(row);
+    },
+  },
+
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1517 弄",
+          zip: 200333,
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1519 弄",
+          zip: 200333,
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1516 弄",
+          zip: 200333,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container /deep/ .el-table .el-table__cell {
+  padding: 2px 0px;
+}
+.container /deep/ ::-webkit-scrollbar{
+  width: 8px;
+  height: 8px;
+  background-color: #FFFFFF;
+}
+.container /deep/  ::-webkit-scrollbar-track{
+  -webkit-box-shadow: inset 0 0 4px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  background-color: #FFFFFF;
+}
+.container /deep/ ::-webkit-scrollbar-thumb{
+  border-radius: 8px;
+  -webkit-box-shadow: inset 0 0 4px rgba(0, 0, 0, 0.2);
+  background-color: #f7fdff;
+}
+</style>
